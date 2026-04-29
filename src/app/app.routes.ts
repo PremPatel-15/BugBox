434b62b7363fa70c26f7211e513de91697b7af82
Prem Patel <prempatel6021@gmail.com>
2026-04-29 15:19:19 +0530

    complete sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { BugList } from './pages/bug-list/bug-list';
 import { BugForm } from './pages/bug-form/bug-form';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: 'bugList', component: BugList },
+  { path: 'buglist', component: BugList },
   { path: 'add', component: BugForm },
   { path: 'edit/:id', component: BugForm },
+  { path: 'dashboard', component: Dashboard },
 ];
